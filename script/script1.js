95435656db5c4bce7a3e5215a20d335713f91e05
Delyan Ninov <dninov@gmail.com>
2020-11-15 06:53:37 +0200

    test

--- script/script1.js
+++ script/script1.js
@@ -5,24 +5,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let topBar = document.getElementById('topBar');
     let logoTxt = document.getElementById('logoTxt');
     let logoBig = true;
-    window.addEventListener("resize", Resize);
-    let tl = gsap.timeline({
-        scrollTrigger:{
-            trigger: arrow,
-            start: "bottom center"
-        }
-    });
-    tl.to(logo, {duration:1.5, y: -500, ease:Elastic.easeIn.config(0.5, 0.3), onComplete:logoSmall})
 
-    // window.onscroll = function() {
-    //     if (window.scrollY > 0 && logoBig == true) {
-    //        logoBig = false;
-    //        gsap.to(logo, {duration:1.5, y: -500, ease:Elastic.easeIn.config(0.5, 0.3), onComplete:logoSmall})
-    //        gsap.to(logoTxt, {duration:0.5, y: 200, ease:Power1.easeIn})
-    //        gsap.to(arrow, {duration:1, alpha: 0});
-    //        gsap.to(topBar, {duration:1, y: 80, ease:Power1.easeOut});
-    //     }        
-    // };
+    window.addEventListener("resize", Resize);
+    // let tl = gsap.timeline({
+    //     scrollTrigger:{
+    //         trigger: arrow,
+    //         start: "bottom center"
+    //     }
+    // });
+    // tl.to(logo, {duration:1.5, y: -500, ease:Elastic.easeIn.config(0.5, 0.3), onComplete:logoSmall})
+
+    window.onscroll = function() {
+        if (window.scrollY > 0 && logoBig == true) {
+           logoBig = false;
+           gsap.to(logo, {duration:1.5, y: -500, ease:Elastic.easeIn.config(0.5, 0.3), onComplete:logoSmall})
+           gsap.to(logoTxt, {duration:0.5, y: 200, ease:Power1.easeIn})
+           gsap.to(arrow, {duration:1, alpha: 0});
+           gsap.to(topBar, {duration:1, y: 80, ease:Power1.easeOut});
+        }        
+    };
 
     function logoSmall(){
       setSmallLogo();
@@ -30,10 +31,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     function setSmallLogo(){
         if (window.innerWidth < 400) {
             console.log("<400");
-            gsap.set(logo, { scaleX:0.4, scaleY:0.4, y:"-37%",left:"25%", zIndex:99})
+            gsap.set(logo, { scaleX:0.4, scaleY:0.4, y:"-37%",left:"25%", zIndex:50})
         }
         else {
-            gsap.set(logo, { scaleX:0.2, scaleY:0.2, y:"-45%",left:"10%", zIndex:99})
+            gsap.set(logo, { scaleX:0.2, scaleY:0.2, y:"-45%",left:"10%", zIndex:50})
         }
     }
     function Resize(){
