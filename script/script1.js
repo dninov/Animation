87ff4c569436c6a873e47f3299219dc6b404ae76
Delyan Ninov <dninov@gmail.com>
2020-11-15 06:20:25 +0200

    test

--- script/script1.js
+++ script/script1.js
@@ -30,10 +30,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     function setSmallLogo(){
         if (window.innerWidth < 400) {
             console.log("<400");
-            gsap.set(logo, { scaleX:0.4, scaleY:0.4, y:"-37%",left:"25%"})
+            gsap.set(logo, { scaleX:0.4, scaleY:0.4, y:"-37%",left:"25%", zIndex:99})
         }
         else {
-            gsap.set(logo, { scaleX:0.2, scaleY:0.2, y:"-45%",left:"10%"})
+            gsap.set(logo, { scaleX:0.2, scaleY:0.2, y:"-45%",left:"10%", zIndex:99})
         }
     }
     function Resize(){
