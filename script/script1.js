14590ad01d38267ba5021b390b30ecbbacd5cce2
Delyan Ninov <dninov@gmail.com>
2020-11-17 08:42:51 +0200

    logo animation

--- script/script1.js
+++ script/script1.js
@@ -5,36 +5,71 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let topBar = document.getElementById('topBar');
     let logoTxt = document.getElementById('logoTxt');
     let logoBig = true;
-
+    let curScroll = window.scrollY;
     window.addEventListener("resize", Resize);
-    // let tl = gsap.timeline({
-    //     scrollTrigger:{
-    //         trigger: arrow,
-    //         start: "bottom center"
-    //     }
-    // });
-    // tl.to(logo, {duration:1.5, y: -500, ease:Elastic.easeIn.config(0.5, 0.3), onComplete:logoSmall})
+
+    if (curScroll > 0){
+        logoBig = false;
+        gsap.to(logoTxt, {duration:0.5, y: 200, ease:Power1.easeIn})
+        gsap.to(arrow, {duration:1, alpha: 0});
+        gsap.to(topBar, {duration:1, y: 80, ease:Power1.easeOut});
+        logoSmall();
+    }
 
     window.onscroll = function() {
+        console.log(window.scrollY, logoBig);
         if (window.scrollY > 0 && logoBig == true) {
            logoBig = false;
-           gsap.to(logo, {duration:1.5, y: -500, ease:Elastic.easeIn.config(0.5, 0.3), onComplete:logoSmall})
+           gsap.to(logo, {duration:0.8, y: -500, ease:Elastic.easeIn.config(1.1, 0.8), onComplete:logoSmall})
            gsap.to(logoTxt, {duration:0.5, y: 200, ease:Power1.easeIn})
            gsap.to(arrow, {duration:1, alpha: 0});
            gsap.to(topBar, {duration:1, y: 80, ease:Power1.easeOut});
-        }        
+        }
+        else if(window.scrollY == 0 && logoBig == false){
+            console.log("in");
+            logoBig = true;
+            gsap.to(logo, {duration:2, y: "0%",scale: 1, left:"50%", ease: Elastic.easeOut.config(1.2, 0.5)});
+            gsap.to(logoTxt, {delay:0.5, duration:1, y: 0, ease:Power3.easeOut})
+            gsap.to(arrow, {duration:1, alpha: 1});
+            gsap.to(topBar, {duration:1, y: 0, ease:Power1.easeIn});
+        }
     };
 
     function logoSmall(){
+        gsap.set(logo, {clearProps: "left, scale"})
+        if (window.innerWidth < 600) {
+            console.log("<600");
+            gsap.set(logo, { scale:0.4, y:"-50%",left:"25%", zIndex:50})
+        }
+        else if (window.innerWidth < 800){
+            console.log("<800");
+            gsap.set(logo, { scale:0.3, y:"-50%",left:"15%", zIndex:50})
+        }
+        else if (window.innerWidth < 1200){
+            console.log("<1200");
+            gsap.set(logo, { scale:0.2, y:"-50%",left:"10%", zIndex:50})
+        }else{
+            console.log("else");
+            gsap.set(logo, { scale:0.15, y:"-46%",left:"8%", zIndex:50})
+        }
       setSmallLogo();
     }
     function setSmallLogo(){
-        if (window.innerWidth < 400) {
-            console.log("<400");
-            gsap.set(logo, { scaleX:0.4, scaleY:0.4, y:"-37%",left:"25%", zIndex:50})
+         
+        if (window.innerWidth < 600) {
+            console.log("<600");
+            gsap.to(logo, { scale:0.4, y:"-37%",left:"25%", zIndex:50})
         }
-        else {
-            gsap.set(logo, { scaleX:0.2, scaleY:0.2, y:"-45%",left:"10%", zIndex:50})
+        else if (window.innerWidth < 800){
+            console.log("<800");
+            gsap.to(logo, { scale:0.3, y:"-42%",left:"15%", zIndex:50})
+        }
+        else if (window.innerWidth < 1200){
+            console.log("<1200");
+            gsap.to(logo, { scale:0.2, y:"-45%",left:"10%", zIndex:50})
+        }else{
+            console.log("else");
+            gsap.to(logo, { scale:0.15, y:"-46%",left:"8%", zIndex:50})
         }
     }
     function Resize(){
