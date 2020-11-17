83804450a5aa63ba2fdcae5bb7ba3395c537fa35
Delyan Ninov <dninov@gmail.com>
2020-11-17 19:18:44 +0200

    tweenMax update

--- script/script1.js
+++ script/script1.js
@@ -10,47 +10,47 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     if (curScroll > 0){
         logoBig = false;
-        gsap.to(logoTxt, {duration:0.5, y: 200, ease:Power1.easeIn})
-        gsap.to(arrow, {duration:1, alpha: 0});
-        gsap.to(topBar, {duration:1, y: 80, ease:Power1.easeOut});
+        TweenMax.to(logoTxt, 0.5, {y: 200, ease:Power1.easeIn})
+        TweenMax.to(arrow, 1,{ alpha: 0});
+        TweenMax.to(topBar, 1, { y: 80, ease:Power1.easeOut});
         logoSmall();
     }
 
     window.onscroll = function() {
         console.log(window.scrollY, logoBig);
-        if (window.scrollY > 0 && logoBig == true) {
+        if (window.scrollY > 100 && logoBig == true) {
            logoBig = false;
-           gsap.to(logo, {duration:0.8, y: -500, ease:Elastic.easeIn.config(1.1, 0.8), onComplete:logoSmall})
-           gsap.to(logoTxt, {duration:0.5, y: 50, alpha: 0, ease:Power1.easeIn})
-           gsap.to(arrow, {duration:1, alpha: 0});
-           gsap.to(topBar, {duration:1, y: 80, ease:Power1.easeOut});
+           TweenMax.to(logo, 0.8,{y:"-55%", ease:Elastic.easeIn.config(1.1, 0.8), onComplete:logoSmall})
+           TweenMax.to(logoTxt, 0.5,{y: 50, alpha: 0, ease:Power1.easeIn})
+           TweenMax.to(arrow, 1,{ alpha: 0});
+           TweenMax.to(topBar, 1,{y: 80, ease:Power1.easeOut});
         }
         else if(window.scrollY == 0 && logoBig == false){
             console.log("in");
             logoBig = true;
-            gsap.to(logo, {duration:2, y: "0%",scale: 1, left:"50%", ease: Elastic.easeOut.config(1, 0.7) });
-            gsap.to(logoTxt, {delay:0.5, duration:1,alpha:1, y: 0, ease:Power3.easeOut})
-            gsap.to(arrow, {duration:1, alpha: 1});
-            gsap.to(topBar, {duration:1, y: 0, ease:Power1.easeIn});
+            TweenMax.to(logo, 2,{ y: "0%",scale: 1, x:"-50%", ease: Elastic.easeOut.config(1, 0.7) });
+            TweenMax.to(logoTxt, 0.5, {delay:0.5, alpha:1, y: 0, ease:Power3.easeOut})
+            TweenMax.to(arrow, 1, {alpha: 1});
+            TweenMax.to(topBar, 1, {y: 0, ease:Power1.easeIn});
         }
     };
 
     function logoSmall(){
-        gsap.set(logo, {clearProps: "left, scale"})
+        TweenMax.set(logo, {clearProps: "x, scale"})
         if (window.innerWidth < 600) {
             console.log("<600");
-            gsap.set(logo, { scale:0.4, y:"-55%",left:"25%", zIndex:50})
+            TweenMax.set(logo, { scale:0.3, y:"-55%",x:"-80%", zIndex:50})
         }
         else if (window.innerWidth < 800){
             console.log("<800");
-            gsap.set(logo, { scale:0.3, y:"-55%",left:"15%", zIndex:50})
+            TweenMax.set(logo, { scale:0.3, y:"-55%",x:"-80%", zIndex:50})
         }
         else if (window.innerWidth < 1200){
             console.log("<1200");
-            gsap.set(logo, { scale:0.2, y:"-55%",left:"10%", zIndex:50})
+            TweenMax.set(logo, { scale:0.2, y:"-55%",x:"-80%", zIndex:50})
         }else{
             console.log("else");
-            gsap.set(logo, { scale:0.15, y:"-55%",left:"8%", zIndex:50})
+            TweenMax.set(logo, { scale:0.15, y:"-55%",x:"-80%", zIndex:50})
         }
       setSmallLogo();
     }
@@ -58,26 +58,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
          
         if (window.innerWidth < 600) {
             console.log("<600");
-            gsap.to(logo, {duration:1, scale:0.4, y:"-37%",left:"25%", zIndex:50, ease: Elastic.easeOut.config(1.05, 0.8), })
+            TweenMax.to(logo, 1,{scale:0.4, y:"-37%",x:"-80%", zIndex:50, ease: Elastic.easeOut.config(1.05, 0.8), })
         }
         else if (window.innerWidth < 800){
             console.log("<800");
-            gsap.to(logo, {duration:1, scale:0.3, y:"-42%",left:"15%", zIndex:50, ease: Elastic.easeOut.config(1.05, 0.8), })
+            TweenMax.to(logo, 1,{ scale:0.3, y:"-42%",x:"-80%", zIndex:50, ease: Elastic.easeOut.config(1.05, 0.8), })
         }
         else if (window.innerWidth < 1200){
             console.log("<1200");
-            gsap.to(logo, {duration:1, scale:0.2, y:"-45%",left:"10%", zIndex:50, ease: Elastic.easeOut.config(1.05, 0.8), })
+            TweenMax.to(logo, 1,{scale:0.2, y:"-45%",x:"-80%", zIndex:50, ease: Elastic.easeOut.config(1.05, 0.8), })
         }else{
             console.log("else");
-            gsap.to(logo, {duration:1, scale:0.15, y:"-46%",left:"8%", zIndex:50, ease: Elastic.easeOut.config(1.05, 0.8), })
+            TweenMax.to(logo, 1,{scale:0.15, y:"-46%",x:"-80%", zIndex:50, ease: Elastic.easeOut.config(1.05, 0.8), })
         }
     }
     function Resize(){
         if (logoBig == false) {
+            console.log("resize logo Small");
           setSmallLogo();  
         } 
         else{
-            gsap.to(logo, {duration:2, y: "0%",scale: 1, left:"50%", ease: Elastic.easeOut.config(1.2, 0.5), });
+            console.log("resize logo Big");
+            TweenMax.set(logo, {clearProps: "left, scale"})
+            // TweenMax.to(logo, 2, { y: "0%",scale: 1, left:"50%"});
         }
     }
 
